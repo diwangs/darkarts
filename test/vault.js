ecb3d85ba8de291a4c717b87cd4cafa25877905f
diwangs <diw@n.gs>
2022-01-17 01:19:21 -0500

    Migrated from SnarkJS to ZoKrates

--- test/vault.js
+++ test/vault.js
@@ -1,19 +1,14 @@
 const MERKLE_TREE_HEIGHT = 20
 
-const crypto = require('crypto')
+const fs = require('fs')
+const merkleTree = require('fixed-merkle-tree')
+const { initialize } = require('zokrates-js/node')
+const truffleAssert = require('truffle-assertions')
+
+// not really needed?
 const circomlib = require('circomlib')
 const snarkjs = require('snarkjs')
 const BigNumber = require('bignumber.js')
-
-// for withdrawal
-const buildGroth16 = require('websnark/src/groth16')
-const websnarkUtils = require('websnark/src/utils')
-const merkleTree = require('fixed-merkle-tree')
-const fs = require('fs')
-const stringifyBigInts = require('websnark/tools/stringifybigint').stringifyBigInts
-
-// for tests
-const truffleAssert = require('truffle-assertions')
 
 const Token = artifacts.require("UserInfo")
 const Vault = artifacts.require("Vault")
@@ -21,22 +16,25 @@ const Vault = artifacts.require("Vault")
 contract("Vault", accounts => {
     let vault;
     let token;
-    let groth16;
+    
+    let sendArtifact;
+    let sendProvingKey;
 
-    let withdraw_circuit;
-    let withdraw_proving_key;
-
-    let send_circuit;
-    let send_proving_key;
+    let withdrawArtifact;
+    let withdrawProvingKey;
 
     before(async () => {
-        groth16 = await buildGroth16();
-
-        withdraw_circuit = require(__dirname + '/../build/circuits/withdraw.json');
-        withdraw_proving_key = fs.readFileSync(__dirname + '/../build/circuits/withdraw_proving_key.bin').buffer;
-
-        send_circuit = require(__dirname + '/../build/circuits/send.json');
-        send_proving_key = fs.readFileSync(__dirname + '/../build/circuits/send_proving_key.bin').buffer;
+        sendArtifact = readArtifact(
+            __dirname + "/../build/circuits/send/program.bin", 
+            __dirname + "/../build/circuits/send/abi.json"
+        )
+        sendProvingKey = readProvingKey(__dirname + "/../build/circuits/send/proving_key.bin")
+        
+        withdrawArtifact = readArtifact(
+            __dirname + "/../build/circuits/withdraw/program.bin", 
+            __dirname + "/../build/circuits/withdraw/abi.json"
+        )
+        withdrawProvingKey = readProvingKey(__dirname + "/../build/circuits/withdraw/proving_key.bin")
     })
 
     beforeEach(async () => {
@@ -49,37 +47,28 @@ contract("Vault", accounts => {
         const result = await token.awardItem.call(accounts[0], "Some URI")
         await token.awardItem(accounts[0], "Some URI")
 
+        // String fixtures (for ZoKrates)
+        const secretId1 = "254080219567091772673909445246567392088208036796494585251815984474602972369"
+        const secretId2 = "381081628386906533194738143309184053498881367127906410807449526152710014129"
+        const nullifier1 = "272418632970930087013102078582984595767243027035861303396446195359448020486"
+        const nullifier2 = "218278570709644582400354933544764600979629809596418196314625408049795099009"
         const tokenAddressHex = token.address.toLowerCase()
         const vaultAddressHex = vault.address.toLowerCase()
         const tokenAddressBN = new BigNumber(tokenAddressHex.slice(2), 16)
-        const n1 = snarkjs.bigInt("272418632970930087013102078582984595767243027035861303396446195359448020486")
-        const s1 = snarkjs.bigInt("254080219567091772673909445246567392088208036796494585251815984474602972369")
-        const n2 = snarkjs.bigInt("218278570709644582400354933544764600979629809596418196314625408049795099009")
-        const s2 = snarkjs.bigInt("381081628386906533194738143309184053498881367127906410807449526152710014129")
-        const toyUidId = snarkjs.bigInt(result.toString())
-        const toyUidContract = snarkjs.bigInt(tokenAddressBN.toString(10))
+        const tokenUidContract = tokenAddressBN.toString(10)
+        const tokenUidId = result.toString()
 
-        const deposit1 = createDeposit({ 
-            nullifier: n1, 
-            secretId: s1,
-            tokenUidId: toyUidId,
-            tokenUidContract: toyUidContract,
-        })
-
-        const deposit2 = createDeposit({ 
-            nullifier: n2, 
-            secretId: s2,
-            tokenUidId: toyUidId,
-            tokenUidContract: toyUidContract,
-        })
+        // Create deposit object
+        const deposit1 = createDeposit(secretId1, nullifier1, tokenUidContract, tokenUidId)
+        const deposit2 = createDeposit(secretId2, nullifier2, tokenUidContract, tokenUidId)
 
         // Approve
-        const tokenIdHex = toHex(toyUidId)
+        const tokenIdHex = toHex(tokenUidId)
         await token.approve(vaultAddressHex, tokenIdHex, {from: accounts[0]})
 
         // Deposit
         console.log("Deposit") 
-        let tx = await vault.deposit(toHex(deposit1.commitment), toHex(toyUidId), tokenAddressHex, {from: accounts[0]})
+        let tx = await vault.deposit(toHex(deposit1.commitment), toHex(tokenUidId), tokenAddressHex, {from: accounts[0]})
 
         truffleAssert.eventEmitted(tx, 'Deposit', (e) => {
             return e.commitment == toHex(deposit1.commitment)
@@ -87,14 +76,7 @@ contract("Vault", accounts => {
 
         // Send
         console.log("Send") 
-        const r1 = await generateSendProof({
-            vault,
-            oldDeposit: deposit1,
-            newDeposit: deposit2,
-            groth16: groth16,
-            circuit: send_circuit,
-            proving_key: send_proving_key
-        })
+        const r1 = await generateSendProof(vault, deposit1, deposit2, sendArtifact, sendProvingKey)
         tx = await vault.send(r1.proof, ...r1.args, {from: accounts[0]})
 
         truffleAssert.eventEmitted(tx, 'Withdrawal', (e) => {
@@ -106,13 +88,7 @@ contract("Vault", accounts => {
 
         // Withdraw
         console.log("Withdraw") 
-        const r2 = await generateWithdrawProof({
-            vault,
-            deposit: deposit2,
-            groth16: groth16,
-            circuit: withdraw_circuit,
-            proving_key: withdraw_proving_key
-        })
+        const r2 = await generateWithdrawProof(vault, deposit2, withdrawArtifact, withdrawProvingKey)
         tx = await vault.withdraw(r2.proof, ...r2.args, {from: accounts[1]})
         
         truffleAssert.eventEmitted(tx, 'Withdrawal', (e) => {
@@ -124,28 +100,69 @@ contract("Vault", accounts => {
 // ---
 // Util functions
 // ---
-const rbigint = nbytes => snarkjs.bigInt.leBuff2int(crypto.randomBytes(nbytes))
-const pedersenHash = data => circomlib.babyJub.unpackPoint(circomlib.pedersenHash.hash(data))[0]
+const mimcSponge = input => circomlib.mimcsponge.multiHash(input, undefined, 1).toString()
 function toHex(number, length = 32) {
     const str = number instanceof Buffer ? number.toString('hex') : snarkjs.bigInt(number).toString(16)
     return '0x' + str.padStart(length * 2, '0')
 }
 
-function createDeposit({ nullifier, secretId, tokenUidId, tokenUidContract }) {
-    // tokenUid = concat(address, tokenId) -> 20 + 32 = 52 bytes
-    const deposit = { nullifier, secretId, tokenUidId, tokenUidContract }
-    deposit.publicId = pedersenHash(deposit.secretId.leInt2Buff(31))
-    deposit.preimage = Buffer.concat([
-        deposit.nullifier.leInt2Buff(31), 
-        deposit.publicId.leInt2Buff(32), 
-        deposit.tokenUidId.leInt2Buff(32),
-        deposit.tokenUidContract.leInt2Buff(20),
-    ])
-    deposit.commitment = pedersenHash(deposit.preimage)
-    deposit.commitmentHex = toHex(deposit.commitment)
-    deposit.nullifierHash = pedersenHash(deposit.nullifier.leInt2Buff(31))
-    deposit.nullifierHex = toHex(deposit.nullifierHash)
+function readArtifact(programPath, abiPath) {
+    const program = Uint8Array.from(fs.readFileSync(programPath))
+    const abi = fs.readFileSync(abiPath, "utf-8")
+    return { program, abi }
+}
+
+function readProvingKey(path) {
+    return Uint8Array.from(fs.readFileSync(path))
+}
+
+function createDeposit(secretId, nullifier, tokenUidContract, tokenUidId) {
+    const deposit = { secretId, nullifier, tokenUidContract, tokenUidId }
+    
+    deposit.publicId = mimcSponge([secretId])
+    deposit.nullifierHash = mimcSponge([nullifier])
+    deposit.commitment = mimcSponge([nullifier, deposit.publicId, tokenUidId, tokenUidContract])
+
     return deposit
+}
+
+async function generateSendProof(vault, oldDeposit, newDeposit, artifact, provingKey) {
+    // Compute merkle proof of our commitment
+    const { root, pathElements, pathIndices } = await generateMerkleProof(vault, oldDeposit)
+
+    // Prepare circuit input
+    const input = [
+        // Public snark inputs
+        root,
+        oldDeposit.nullifierHash,
+        newDeposit.commitment,
+    
+        // Private snark inputs
+        oldDeposit.nullifier,
+        oldDeposit.secretId,
+        oldDeposit.tokenUidId,
+        oldDeposit.tokenUidContract,
+        pathElements,
+        pathIndices.map(Boolean),
+        newDeposit.nullifier,
+        newDeposit.publicId,
+        newDeposit.tokenUidId,
+        newDeposit.tokenUidContract,
+    ]
+    
+    // generate witness and prove
+    console.log('Generating SNARK proof')
+    console.time('Proof time')
+    const proof = await generateSnarkProof(artifact, provingKey, input)
+    console.timeEnd('Proof time')
+
+    const args = [
+        toHex(root),
+        toHex(oldDeposit.nullifierHash),
+        toHex(newDeposit.commitment),
+    ]
+
+    return { proof, args }
 }
 
 /**
@@ -156,76 +173,36 @@ function createDeposit({ nullifier, secretId, tokenUidId, tokenUidContract }) {
  * @param fee Relayer fee
  * @param refund Receive ether for exchanged tokens
  */
- async function generateWithdrawProof({ vault, deposit, groth16, circuit, proving_key }) {
+async function generateWithdrawProof(vault, deposit, artifact, provingKey) {
     // Compute merkle proof of our commitment
     const { root, pathElements, pathIndices } = await generateMerkleProof(vault, deposit)
-  
+
     // Prepare circuit input
-    const input = stringifyBigInts({
-      // Public snark inputs
-      root: root,
-      nullifierHash: deposit.nullifierHash,
-      tokenUidId: deposit.tokenUidId,
-      tokenUidContract: deposit.tokenUidContract,
-  
-      // Private snark inputs
-      nullifier: deposit.nullifier,
-      secretId: deposit.secretId,
-      pathElements: pathElements,
-      pathIndices: pathIndices,
-    })
+    const input = [
+        // Public snark inputs
+        root,
+        deposit.nullifierHash,
+        deposit.tokenUidId,
+        deposit.tokenUidContract,
     
-    console.log('Generating SNARK proof')
-    console.time('Proof time')
-    const proofData = await websnarkUtils.genWitnessAndProve(groth16, input, circuit, proving_key)
-    const { proof } = websnarkUtils.toSolidityInput(proofData)
-    console.timeEnd('Proof time')
-  
-    const args = [
-      toHex(input.root),
-      toHex(input.nullifierHash),
-      toHex(input.tokenUidId),
-      toHex(input.tokenUidContract)
+        // Private snark inputs
+        deposit.nullifier,
+        deposit.secretId,
+        pathElements,
+        pathIndices.map(Boolean),
     ]
-
-    return { proof, args }
-}
-
-async function generateSendProof({ vault, oldDeposit, newDeposit, groth16, circuit, proving_key }) {
-    // Compute merkle proof of our commitment
-    const { root, pathElements, pathIndices } = await generateMerkleProof(vault, oldDeposit)
-  
-    // Prepare circuit input
-    const _input = {
-      // Public snark inputs
-      oldRoot: root,
-      oldNullifierHash: oldDeposit.nullifierHash,
-      newCommitment: newDeposit.commitment,
-  
-      // Private snark inputs
-      oldNullifier: oldDeposit.nullifier,
-      oldSecretId: oldDeposit.secretId,
-      oldTokenUidId: oldDeposit.tokenUidId,
-      oldTokenUidContract: oldDeposit.tokenUidContract,
-      oldPathElements: pathElements,
-      oldPathIndices: pathIndices,
-      newNullifier: newDeposit.nullifier,
-      newPublicId: newDeposit.publicId,
-      newTokenUidId: newDeposit.tokenUidId,
-      newTokenUidContract: newDeposit.tokenUidContract,
-    }
-    const input = stringifyBigInts(_input)
     
+    // generate witness and prove
     console.log('Generating SNARK proof')
     console.time('Proof time')
-    const proofData = await websnarkUtils.genWitnessAndProve(groth16, input, circuit, proving_key)
-    const { proof } = websnarkUtils.toSolidityInput(proofData)
+    const proof = await generateSnarkProof(artifact, provingKey, input)
     console.timeEnd('Proof time')
-  
+
     const args = [
-      toHex(_input.oldRoot),
-      toHex(_input.oldNullifierHash),
-      toHex(_input.newCommitment),
+        toHex(root),
+        toHex(deposit.nullifierHash),
+        toHex(deposit.tokenUidId),
+        toHex(deposit.tokenUidContract)
     ]
 
     return { proof, args }
@@ -260,4 +237,11 @@ async function generateSendProof({ vault, oldDeposit, newDeposit, groth16, circu
     // Compute merkle proof of our commitment
     const { pathElements, pathIndices } = tree.path(leafIndex)
     return { pathElements, pathIndices, root: tree.root() }
+}
+
+async function generateSnarkProof(artifacts, pk, input) {
+    zokratesProvider = await initialize()
+    const { witness, output } = zokratesProvider.computeWitness(artifacts, input);
+    const proofRaw = zokratesProvider.generateProof(artifacts.program, witness, pk);
+    return Object.values(proofRaw.proof)
 }
