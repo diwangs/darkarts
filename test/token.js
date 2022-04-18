84c39ce3d8fa4279472cb932fd77036939bb3a44
diwangs <diw@n.gs>
2022-04-18 04:57:16 -0400

    Added Waffle test for dummy token

--- test/token.js
+++ test/token.js
@@ -1,47 +1,24 @@
-const DummyNFT = artifacts.require("UserInfo");
+const { expect } = require("chai");
+const { ethers } = require("hardhat")
 
-contract("DummyNFT", accounts => {
+describe("Dummy Token", function() {
     let nft;
-    let userAccount;
+    let owner, addr1, addr2;
 
-    beforeEach(async () => {
-        nft = await DummyNFT.deployed();
-        userAccount = accounts[0];
+    beforeEach(async function() {
+        const DummyNFT = await ethers.getContractFactory("UserInfo")
+        nft = await DummyNFT.deploy();
+        await nft.deployed();
+        [owner, addr1, addr2] = await ethers.getSigners()
     })
-
-    it("Test ERC721 implementation", async () => {
-        let result = await nft.awardItem(userAccount, "Some URL", { from: accounts[0] });
-        let tokenId = result.logs[0].args.tokenId;
-        
-        let owner = await nft.ownerOf(tokenId);
-        assert.equal(owner, userAccount, "User account doesn't have a token");
-
-        let tokenURI = await nft.tokenURI(tokenId);
-        assert.equal(
-            "Some URL",
-            tokenURI,
-            "Token metadata is wrong"
-        );
-
-        let operator = await nft.getApproved(tokenId);
-        assert.notEqual(
-            operator,
-            userAccount,
-            "User account doesn't get approved"
-        );
-
-        let name = await nft.name()
-        assert.equal(
-            "UserInfo",
-            name,
-            "Incorrect token collection name"
-        );
-
-        let symbol = await nft.symbol()
-        assert.equal(
-            "ITM",
-            symbol,
-            "Incorrect token collection symbol"
-        );
-    });
-});
+    
+    describe("Tx", function() {
+        it("Should return something", async function () {
+            await nft.awardItem(addr1.address, "Some URL")
+            const addr1Balance = await nft.balanceOf(addr1.address) 
+            // Why doesn't ownerOf work?
+            // https://github.com/TrueFiEng/Waffle/issues/339
+            expect(addr1Balance).to.equal(1)
+        })    
+    })
+})
